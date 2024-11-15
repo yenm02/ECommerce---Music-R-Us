27b7ba53e2195802ba485866cadf1fa21b9996d5
Alessio Naji <alessionaji1@gmail.com>
2024-11-14 18:12:36 -0600

    Update user.h

--- user.h
+++ user.h
@@ -1,8 +1,10 @@
-// header file
+#ifndef USER_H
+#define USER_H
 
 #include <iostream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 class Playlist;
@@ -12,29 +14,28 @@ class Merchandise;
 
 // User class
 class User {
-    public: 
+public:
+    // Attributes
+    int user_id;                           // identifier for the user
     string userName;
     string password;
     string email;
     string subscriptionStatus;
-    vector<Music> personalizedRecc;
-    vector<Playlist> playlists;
-    vector<Order> orderStatus;
-    vector<Music> listeningHistory;
-    vector<Merchandise> wishList;
+    vector<Music> personalizedRecc;        // List of recommended music
+    vector<Playlist> playlists;            // User's playlists
+    vector<Order> orderStatus;             // List of user's orders
+    vector<Music> listeningHistory;        // User's listening history
+    vector<Merchandise> wishList;          // Wishlist of merchandise
 
-/// constructor for default values
-    User(string uName, string userPass, string emailAD)
-        : userName(uName), password(userPass), email(emailAD) {}
+    // Constructor with default subscription status set to free
+    User(int id, string uName, string userPass, string emailAD, string subStatus = "Free")
+        : user_id(id), userName(uName), password(userPass), email(emailAD), subscriptionStatus(subStatus) {}
 
-;
-    /// class methoxds
-    void getUserDetails();
-    void updateProfile(string newUserData);
-    void viewPurchaseHistory ();
-    void createPlaylist (string playlistName);
+    // Methods
+    void getUserDetails();                  // Displays user details
+    void updateProfile(string newUserData); // Updates user's profile with new email
+    void viewPurchaseHistory();             // Views purchase history
+    void createPlaylist(string playlistName); // Creates a new playlist
 };
 
-
-
-
+#endif // USER_H
